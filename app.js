519e7e8045c530fc3855ab3cc9a58db98244bbde
Maria Dahlquist <maria.dahlquist@svenskaspel.se>
2017-09-27 15:19:27 +0200

    added more pages and css

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express')
+const path = require('path')
 const app=express()
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
 	console.log('Snart lunch')
-	res.send('hello world')
+	res.render('fruit',  { fruit: 'mango' })
+	//res.send('hello world')
 
 })
 
@@ -12,13 +18,17 @@ app.get('/om-mig', (req, res) => {
 	res.send('Om mej')
 
 })
+app.get('/ananas', (req, res) => {
+	console.log('ananas')
+	res.render('fruit',  { fruit: 'ananas' })
 
+})
 app.get('/kontakt', (req, res) => {
 	console.log('Kontakt')
 	res.send('Ni mår mej på maria.dahlquist@svenskaspel.se')
 
 })
-app.listen(3006,  (req, res) =>  {
+app.listen(3018,  (req, res) =>  {
 
 	console.log ('App acting on port 3000. Visit localhost:3000 in chrome')
 
